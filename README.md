8faa7e5ee343c7cdf6b5a0523c5b2bbea5b58d4a
Blaze <orsotreo@gmail.com>
2023-11-22 07:55:54 +0300

    sprint-6/step-1 cypress, забыл извините, файл не стал разбивать т.к не очень много кода вышло

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Getting Started with Create React App
-
+Project url -  [stellar burger](https://alexandrblaze.github.io/react-burger/).\
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,45 @@
+describe('The Home Page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.intercept("POST", "api/orders", { fixture: "order.json" }).as("postOrder");
+    cy.get('[data-cy="orderSubmit"]').as("orderSubmit");
+    cy.get('[data-cy="ingredients"]').as("items");
+    cy.get('[data-cy="dropContainer"]').as("drop");
+
+    cy.get("@items").children().first().as("bun");
+    cy.get("@items").children().last().as("main");
+    cy.get("@items").children().eq(2).as("sauce");
+
+    // Устанавливаем токены:
+    window.localStorage.setItem(
+        "refreshToken",
+        JSON.stringify("test-refreshToken")
+    );
+    cy.setCookie('accessToken', 'test-accessToken')
+  });
+
+  it("create order", function () {
+    cy.get("@orderSubmit").click();
+    cy.intercept("POST", "api/auth/login", { fixture: "loginData.json" }).as(
+        "loginData"
+    );
+
+    cy.get("[name=email]").type(`test@mail.com`);
+    cy.get("[name=password]").type(`123456`);
+    cy.contains("Войти").click();
+    cy.intercept("GET", "api/auth/user", { fixture: "user.json" }).as("user");
+
+    cy.get("@bun").trigger("dragstart");
+    cy.get("@drop").trigger("drop");
+    cy.get("@sauce").trigger("dragstart");
+    cy.get("@drop").trigger("drop");
+    cy.get("@main").trigger("dragstart");
+    cy.get("@drop").trigger("drop");
+
+    cy.get("@orderSubmit").click();
+
+    cy.intercept("POST", "api/orders", { fixture: "order.json" }).as(
+        "postOrder"
+    );
+  });
+})
